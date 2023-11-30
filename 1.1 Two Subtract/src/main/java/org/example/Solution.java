44fc1b6170b15062ac35a4f0312e07aa476c6c8a
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-11-30 22:39:50 +0530

    Add files via upload

---
+++ 1.1 Two Subtract/src/main/java/org/example/Solution.java
@@ -0,0 +1,17 @@
+package org.example;
+public class Solution {
+    public int[] twoSubtract(int[] nums, int target){
+        for(int i=0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] - nums[j]) == target){
+                    return new int[] {i,j};
+                }
+                else if ((nums[j] - nums[i]) == target){
+                    return new int[] {i,j};
+                }
+
+            }
+        }
+        return null;
+    }
+}
